2c7ec39b0983f59268cc33a1ccc9efde5227e833
almas-alright <kutsnalmas@gmail.com>
2020-11-26 16:48:18 +0600

    General Info Name Validation

--- plugins/validation.js
+++ plugins/validation.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import * as VeeValidate from 'vee-validate';
 import { ValidationProvider, ValidationObserver, extend} from 'vee-validate';
-import { required, min, regex } from "vee-validate/dist/rules";
+import { required, min, regex, alpha_spaces } from "vee-validate/dist/rules";
 Vue.use(VeeValidate);
 Vue.component('ValidationProvider', ValidationProvider);
 Vue.component("ValidationObserver", ValidationObserver);
@@ -11,3 +11,4 @@ extend("required", required);
 extend("min", min);
 
 extend("regx", regex);
+extend("alpha_spaces", alpha_spaces);
