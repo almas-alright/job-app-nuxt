abb3b51d602cb850ff802281ffec7718ac64dc43
almas-alright <kutsnalmas@gmail.com>
2020-12-14 16:16:22 +0600

    Email Verify

--- components/auth/VerifyEmail.vue
+++ components/auth/VerifyEmail.vue
@@ -2,19 +2,18 @@
   <div>
     <div class="row">
       <div class="container">
-        <div class="col-12">
+        <div v-if="submitted" class="col-12 bg-success text-center p-2">
+          <p>{{ msg }}</p>
+          <div v-if="showLoader" class="text-center">
+            <b-spinner variant="primary" type="grow" label="Spinning"></b-spinner>
+          </div>
+        </div>
+        <div v-if="!submitted" class="col-12">
           <div class="text-danger text-center" v-if="lv_errors.abc">{{ lv_errors.abc[0] }}</div>
-          <!-- <input type="text" placeholder="Enter Username" id="uname" :class="{'is-invalid' : lv_errors.name }" v-model="form.name" required>
-            <div class="invalid-feedback" v-if="lv_errors.name">{{ lv_errors.name[0] }}</div> -->
-          <input type="text" disabled placeholder="Enter Email" id="email" :class="{'is-invalid' : lv_errors.email }"
+          <input type="text" placeholder="Enter Email" id="email" :class="{'is-invalid' : lv_errors.email }"
                  v-model="form.email" required>
           <div class="invalid-feedback" v-if="lv_errors.email">{{ lv_errors.email[0] }}</div>
-          <!-- <label for="psw"><b>Password</b></label> -->
-          <!-- <input type="password" placeholder="Enter Password" id="psw" :class="{'is-invalid' : lv_errors.password }" v-model="form.password" required>
-          <input type="password" placeholder="Enter Password again" id="pswc" :class="{'is-invalid' : lv_errors.password }" v-model="form.password_confirmation" required> -->
-          <div class="invalid-feedback" v-if="lv_errors.password">{{ lv_errors.password[0] }}</div>
-<!--          <div class="text-center" v-if="showLoader"><b-spinner type="grow" label="Spinning"></b-spinner></div>-->
-          <button type="button" v-on:click="submit()">Sign Up</button>
+          <button type="button" v-on:click="submit()">Send</button>
         </div>
       </div>
     </div>
@@ -23,75 +22,44 @@
 
 <script>
 import _ from 'lodash'
+
 export default {
   data() {
 
     return {
+      submitted: false,
+      msg: "",
       showLoader: false,
       form: {
-        name: '--',
         email: '',
-        password: '',
-        password_confirmation: ''
       }
     }
 
   },
   methods: {
     submit() {
+      this.submitted = true
       this.showLoader = true
-      let pass = this.randomString(10, 'an')
-      this.form.password = pass
-      this.form.password_confirmation = pass
       let that = this
-      this.$axios.$post('/auth/register', that.form)
+      this.$axios.$patch('/auth/send-link', that.form)
         .then(function (response) {
-          that.showLoader = false
-          if(response.success){
-            that.login()
+          if (response.success) {
+            that.showLoader = false
+            that.msg = response.message
           }
         })
         .catch(function (error) {
           console.log(error);
-        });
-    },
-    randomString(len, an) {
-      an = an && an.toLowerCase();
-      var str = "",
-        i = 0,
-        min = an == "a" ? 10 : 0,
-        max = an == "n" ? 10 : 62;
-      for (; i++ < len;) {
-        var r = Math.random() * (max - min) + min << 0;
-        str += String.fromCharCode(r += r > 9 ? r < 36 ? 55 : 61 : 48);
-      }
-      return str;
-    },
-    login(){
-      let that = this
-      this.form.active = '1'
-      this.$auth.login({ data: that.form })
-        // that.$router.push({ name : 'user-profile'})
-        .then(function (response) {
-          if(response.data.success){
-            that.$router.push({ name : 'my-profile'})
-
+        }).finally(function () {
+          if(typeof that.lv_errors.email != "undefined" ){
+            that.showLoader = false
+            that.submitted = false
           }
-        })
-    }
+      });
+    },
 
   },
-  // computed:{
-  //   hasError(){
-  //     if(_.isEmpty(this.lv_errors)){
-  //       return false
-  //     }
-  //   }
-  // }
-  mounted() {
-    this.form.email = atob(this.$route.params.hash);
-    console.log(this.$route.params.otp)
-  }
+
 }
 </script>
 
---
+++ pages/Register/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="home-section section-hero overlay bg-image" :style="{ backgroundImage: 'url(' + require('@/assets/images/hero_1.jpg') + ')' }" id="home-section">
+
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-12">
+            <div class="mb-5 text-center">
+              <h1 v-if="!authenticated" class="text-white font-weight-bold">Get Registration Link</h1>
+              <h1 v-if="authenticated" class="text-white font-weight-bold">!</h1>
+            </div>
+            <div class="search-jobs-form">
+              <div class="row mb-5">
+                <div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0">
+
+                </div>
+                <div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                  <verify-email v-if="!authenticated" />
+                </div>
+                <div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </section>
+</template>
+
+<script>
+import VerifyEmail from '~/components/auth/VerifyEmail'
+export default {
+  layout: 'main',
+  // middleware: 'auth',
+  components:{VerifyEmail},
+  head() {
+    return {
+      title: 'register | stevejobs',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'stevejobs is a job searching and candidate hosting application'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped >
+
+</style>
