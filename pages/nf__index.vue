6de1d586f549862f0d6f72f4379bd7da37f28adc
almas-alright <kutsnalmas@gmail.com>
2021-03-03 16:52:32 +0600

    minor changes

---
+++ pages/nf__index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <section class="home-section-board">
+
+    </section>
+    <board></board>
+  </div>
+
+</template>
+
+<script>
+import Board from '~/components/job/Board';
+export default {
+  layout: 'main',
+  components:{ Board },
+  data(){
+    return {
+      someImg: ''
+    }
+  },
+  methods:{
+  },
+  head() {
+    return {
+      title: 'home | stevejobs',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'stevejobs is a job searching and candidate hosting application'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home-section-board{
+  min-height: 100px;
+}
+
+</style>
